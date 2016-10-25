c11fde3995ac34f91d6736a84975e4b710c9d0de
tedster827 <tedster827@gmail.com>
2016-10-25 09:48:21 -0700

    Adding More Functions
    Adding the skeleton code for evalSlope and evalYInt. They will evaluate
    the slope and the y-intercept respectably.

--- client.cpp
+++ client.cpp
@@ -1,10 +1,50 @@
-#include "Graph.h"
+//#include "Graph.h"
+#include <iostream>
+#include <vector>
+#include <string>
+
 using namespace std;
 
+//
+typedef char ELM;
+/*Adding the option to change the precison of slope.
+and y-intercept*/
+typedef int slope;
+typedef int yInt;
+
+slope evalSlope(string equation)
+{
+  slope equSlope;
+  return equSlope;
+}
+
+yInt evalYInt(string equation)
+{
+  yInt equYInt;
+  return equYInt;
+}
 
 
 int main()
 {
-  //funtionInput()
+  int xAxisScale = 100;
+  cout << "x - Axis Scale: " << xAxisScale << endl;
+  string equ = "y = x";
+  cout << "Graphing: " << equ << endl;
+  slope slopeFun = evalSlope(equ);
+  yInt yIntFun = evalYInt(equ);
+
+
+
+/*
+  for(int i = 0; i > xAxisScale; i++)
+  {
+    vector<ELM> xAxis
+  }
+*/
+
+  //vector<ELM> xAxis;
+  //vector<xAxis> yAxis;
+
   return 0;
 }
