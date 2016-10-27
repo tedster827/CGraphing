ae5c673c374dce0c9a9c8f31d2f7a9ae3f7ea43c
tedster827 <tedster827@gmail.com>
2016-10-27 12:10:41 -0700

    Slope Function Somewhat Functional
    The Slope Function will evaluate the slope of  an equation in the form
    y = mx + b.
    Found a bug with equations in these forms:
    y=b+mx
    y= (m with spacing) + b

--- client.cpp
+++ client.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 #include <string>
 #include <sstream>
+#include <math.h>
 //#include <stdio.h>
 
 using namespace std;
@@ -23,7 +24,7 @@ slope evalSlope(string &equation)
   //Instantiating a slope for a return value of the function.
   slope equSlope;
   //True for positive, false for negative
-  bool sign;
+  bool sign = true;
   cout << "Evaulting the Slope for ";
   for(int i = 0; i < equation.size(); i++)
   {
@@ -38,7 +39,6 @@ slope evalSlope(string &equation)
     {
       if(equation[i] == 'x')
       {
-        cout << "Tracer 1: " << equation[i] << endl;
         for(int j = i; j > 0; j--)
         {
           if(equation[j] == '-')
@@ -48,10 +48,23 @@ slope evalSlope(string &equation)
           }
           if(isdigit(equation[j]))
           {
-            cout << "Tracer 2: " << equation[j] << endl;
+            cout << "Tracer 3: " << equation[j] << endl;
             //Need to convert to integer from string index
-            equSlope = (int)equation[j];
-            cout << "Tracer 3: " << equSlope << endl;
+            equSlope = equation[j] - 48;
+            cout << "Tracer 4: " << equSlope << endl;
+            int powerOfTen = 1;
+            while(isdigit(equation[j-1]))
+            {
+              j--;
+              cout << "Current Index: " << j << endl;
+              cout << "Current Digit: " << equation[j] << endl;
+              int num = equation[j];
+              num = num - 48;
+              cout << "Evaluation : " << num << "* (10" << "^" << powerOfTen << ")" << endl;
+              equSlope = equSlope + ((num) * pow(10, powerOfTen));
+              powerOfTen++;
+            }
+            cout << "Tracer 5: " << equSlope - 2 << endl;
           }
           else if(equation[j - 1] == '+' || equation[j - 1] == '-')
           {
@@ -84,7 +97,7 @@ int main()
   int yAxisScale = 100;
   cout << "x - Axis Scale: " << xAxisScale << endl;
   cout << "y - Axis Scale: " << yAxisScale << endl;
-  string equ = "y = 3x"; // + " ."; Was going to add a end marker, not needed anymore.
+  string equ = "y = 8457865x + 2"; // + " ."; Was going to add a end marker, not needed anymore.
   cout << "Graphing: " << equ << endl;
   slope equSlope = evalSlope(equ);
   cout << "Slope: " << equSlope << endl;
